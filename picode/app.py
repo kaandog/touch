330a8e24a8a06bcba613a7064936d3400f400792
Raghav <raghavg@andrew.cmu.edu>
2016-03-19 19:13:19 +0000

    Integrated with serial write

--- picode/app.py
+++ picode/app.py
@@ -1,16 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+import time
+import serial
+
+ser = serial.Serial(
+       port='/dev/ttyAMA0',
+       baudrate = 115200,
+       parity=serial.PARITY_NONE,
+       stopbits=serial.STOPBITS_ONE,
+       bytesize=serial.EIGHTBITS,
+       timeout=1
+       )
+
 @app.route('/up/<usr>')
 def up(usr):
     s = "UP : " + str(usr)
     print s
+    ser.write("5")
     return s
 
 @app.route('/down/<usr>')
 def down(usr):
     s = "DOWN : " + str(usr)
     print s
+    ser.write("1")
     return s
 
 if __name__ == '__main__':
